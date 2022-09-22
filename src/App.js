acda51cf489c3007462fabd4f27cdc784fcd2266
AnyaRatusnyak <anyaaboyko@gmail.com>
2022-09-22 22:46:10 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Converter App</div>;
+  return (
+    <div className="App">
+      <div className="container"></div>
+    </div>
+  );
 }
